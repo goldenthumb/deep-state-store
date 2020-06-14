4549e8aa59596ccc62c7ee366c3e0320a590108c
yhchoi <cyh8914@gmail.com>
2020-06-14 17:03:36 +0900

    feat: add deep equal and defer

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,74 @@
 import Store from './index';
 
 describe('Store', () => {
-    it('create', () => {
+    it('get state', () => {
         const store = new Store<{
             test?: number;
             data?: string;
         }>({ test: 0, data: 'test' });
+
         expect(store.state).toEqual({ test: 0, data: 'test' });
+    });
+
+    it('does not call listener if state is same. (deep equal)', () => {
+        const store = new Store<{
+            test?: number;
+            data?: string[];
+        }>({ test: 3, data: ['test1', 'test2'] });
+
+        let calls = 0;
+
+        store.on(['test', 'data'], () => { calls++; });
+        store.set({ test: 3 });
+        store.set({ data: ['test1', 'test2'] });
+        store.set({ test: 3 });
+        store.set({ data: ['test1', 'test2'] });
+        store.set({ test: 3, data: ['test1', 'test2'] });
+
+        Promise.resolve().then(() => {
+            expect(calls).toBe(0);
+        });
+    });
+
+    it('defer task. (only tasks of the current round will be called.)', () => {
+        const store = new Store<{
+            test?: number;
+            data?: string[];
+        }>({ test: 3, data: ['test1', 'test2'] });
+
+        let calls = 0;
+
+        store.on(['test', 'data'], () => { calls++; });
+        store.set({ test: 1 });
+        store.set({ data: ['test1'] });
+        store.set({ test: 2, data: ['test1', 'test2', 'test3'] });
+        store.set({ test: 3, data: ['test2'] });
+        store.set({ test: 1 });
+        
+        Promise.resolve().then(() => {
+            expect(calls).toBe(1);
+            expect(store.state.test).toEqual(1);
+            expect(store.state.data).toEqual(['test2']);
+        });
+    });
+    
+    it('remove listener', () => {
+        const store = new Store<{
+            test?: number;
+            data?: string[];
+        }>({ test: 3, data: ['test1', 'test2'] });
+        
+        let calls = 0;
+        
+        const clearEvent = store.on(['test'], () => { calls++; });
+        store.on(['test'], () => { calls++; });
+        store.set({ test: 3 });
+        store.set({ test: 4 });
+        store.set({ test: 5 });
+        clearEvent();
+        
+        Promise.resolve().then(() => {
+            expect(calls).toBe(1);
+        });
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import equal from 'deep-equal';
 import Emitter, { Listener } from './EventEmitter';
 
 interface State {
@@ -7,24 +8,38 @@ interface State {
 export default class Store<T extends State> {
     private _state: T;
     private _emitter: Emitter;
+    private _defer: Promise<void> | null;
 
     constructor(initState: T) {
         this._state = initState;
         this._emitter = new Emitter();
+        this._defer = null;
     }
 
     get state() {
         return this._state;
     }
 
-    on<K extends (keyof T & string)>(state: K, listener: Listener) {
-        return this._emitter.on(state, listener);
+    on<K extends (keyof T & string)>(deps: K[], listener: Listener) {
+        const clearEvents = deps.map((dep) => this._emitter.on(dep, () => listener(this._state)));
+
+        return () => {
+            clearEvents.forEach(clearEvent => clearEvent());
+        };
     }
 
-    set(state: T) {
-        this._state = { ...this._state, ...state };
-        for (const [key, value] of Object.entries(state)) {
-            this._emitter.emit(key, value);
+    set(nextState: T) {
+        for (const [key, value] of Object.entries(nextState)) {
+            if (!this._state.hasOwnProperty(key)) throw new Error(`State does not exist. (${key})`);
+            if (equal(this._state[key], value)) continue;
+            this._state = { ...this._state, [key]: value };
+
+            if (!this._defer) {
+                this._defer = Promise.resolve().then(() => {
+                    this._defer = null;
+                    this._emitter.emit(key, this._state[key]);
+                });
+            }
         }
     }
 }
